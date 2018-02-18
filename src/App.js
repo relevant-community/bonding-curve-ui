8e33efba0fcb774273dd2d628aba178d72eb51f0
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-02-17 21:21:01 -0700

    graphs working and account not running away

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BondedToken address="0xf25186b5081ff5ce73482ad761db0eb0d25abfbf"/>
-{/*        <BondedToken />*/}
+{/*        <BondedToken address="0xf25186b5081ff5ce73482ad761db0eb0d25abfbf"/>*/}
+        <BondedToken />
       </div>
     );
   }
--- src/Chart.js
+++ src/Chart.js
@@ -1,14 +1,15 @@
 import React from 'react';
 const Recharts = require('recharts');
-const {AreaChart, Area, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceDot} = Recharts;
+const {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ReferenceDot} = Recharts;
 
 
 class CurveChart extends React.Component {
   render () {
-    if (!this.props.chartData) return;
+    if (!this.props.chartData) return null
     let { data, currentPrice } = this.props.chartData;
-    let sellData = data.map(d => d.sell ? { supply: d.supply, value: d.sell } : null).filter(d => d);
-    let buyData = data.map(d => d.buy ? { supply: d.supply, value: d.buy } : null).filter(d => d);
+    if (!data || !currentPrice) return null
+    // let sellData = data.map(d => d.sell && d.supply ? { supply: d.supply, value: d.sell } : null).filter(d => d);
+    // let buyData = data.map(d => d.buy && d.supply ? { supply: d.supply, value: d.buy } : null).filter(d => d);
     return (
       <div className='chart'>
         <LineChart
